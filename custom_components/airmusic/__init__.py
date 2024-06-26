723b10f81a570e73504c7f205bcb1e805f42ce85
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-26 21:46:57 +0200

    Update __init__.py

--- custom_components/airmusic/__init__.py
+++ custom_components/airmusic/__init__.py
@@ -1,10 +1,12 @@
 import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import discovery
 
 DOMAIN = 'airmusic'
 _LOGGER = logging.getLogger(__name__)
 
-def setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the AirMusic component."""
     if DOMAIN not in config:
         return True
@@ -18,9 +20,26 @@ def setup(hass, config):
         'token': token
     }
 
-    discovery.load_platform(hass, 'media_player', DOMAIN, {}, config)
+    await discovery.async_load_platform(hass, 'media_player', DOMAIN, {}, config)
     return True
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up AirMusic from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, 'media_player')
+    )
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, 'media_player')
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    return True
 
 
 
