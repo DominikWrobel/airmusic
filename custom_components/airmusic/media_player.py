b5b6cb03ebd74e13ed56c6ec1caaa13cdd36412d
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-26 18:45:19 +0200

    Update media_player.py

--- custom_components/airmusic/media_player.py
+++ custom_components/airmusic/media_player.py
@@ -1,154 +1,167 @@
-# custom_components/airmusic/media_player.py
+"""Support for AirMusic as media player."""
 
-import logging
-from homeassistant.components.media_player import (
-    MediaPlayerEntity,
-    PLATFORM_SCHEMA,
-)
-from homeassistant.components.media_player.const import (
-    SUPPORT_PLAY,
-    SUPPORT_PAUSE,
-    SUPPORT_STOP,
-    SUPPORT_VOLUME_SET,
-    SUPPORT_VOLUME_MUTE,
-    SUPPORT_PREVIOUS_TRACK,
-    SUPPORT_NEXT_TRACK,
-)
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_NAME,
-    STATE_IDLE,
-    STATE_PLAYING,
-)
+from __future__ import annotations
+
+from datetime import timedelta
+import time
+
+from clementineremote import ClementineRemote
 import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
 
-_LOGGER = logging.getLogger(__name__)
-
-SUPPORT_AIRMUSIC = (
-    SUPPORT_PLAY
-    | SUPPORT_PAUSE
-    | SUPPORT_STOP
-    | SUPPORT_VOLUME_SET
-    | SUPPORT_VOLUME_MUTE
-    | SUPPORT_PREVIOUS_TRACK
-    | SUPPORT_NEXT_TRACK
+from homeassistant.components.media_player import (
+    PLATFORM_SCHEMA as MEDIA_PLAYER_PLATFORM_SCHEMA,
+    MediaPlayerEntity,
+    MediaPlayerEntityFeature,
+    MediaPlayerState,
+    MediaType,
 )
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_HOST, CONF_NAME, CONF_PORT
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from . import airmusic
 
 DEFAULT_NAME = "AirMusic"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+SCAN_INTERVAL = timedelta(seconds=5)
+
+PLATFORM_SCHEMA = MEDIA_PLAYER_PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_ACCESS_TOKEN): cv.positive_int,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     }
 )
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the AirMusic platform."""
-    if discovery_info is None:
-        return
 
-    host = discovery_info[CONF_HOST]
-    name = discovery_info.get(CONF_NAME, DEFAULT_NAME)
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the Clementine platform."""
 
-    add_entities([AirMusicDevice(name, host)], True)
+    host = config[CONF_HOST]
+    port = config[CONF_PORT]
+    token = config.get(CONF_ACCESS_TOKEN)
+
+    client = ClementineRemote(host, port, token, reconnect=True)
+
+    add_entities([ClementineDevice(client, config[CONF_NAME])])
 
 
-class AirMusicDevice(MediaPlayerEntity):
-    """Representation of an AirMusic device."""
+class ClementineDevice(MediaPlayerEntity):
+    """Representation of Clementine Player."""
 
-    def __init__(self, name, host):
-        """Initialize the AirMusic device."""
-        self._name = name
-        self._host = host
-        self._state = STATE_IDLE
-        self._volume_level = 0.5
-        self._is_muted = False
+    _attr_media_content_type = MediaType.MUSIC
+    _attr_supported_features = (
+        MediaPlayerEntityFeature.PAUSE
+        | MediaPlayerEntityFeature.VOLUME_STEP
+        | MediaPlayerEntityFeature.PREVIOUS_TRACK
+        | MediaPlayerEntityFeature.VOLUME_SET
+        | MediaPlayerEntityFeature.NEXT_TRACK
+        | MediaPlayerEntityFeature.SELECT_SOURCE
+        | MediaPlayerEntityFeature.PLAY
+    )
 
-        # Initialization of the device with provided script
-        # self._device = YourProvidedScript(host)
+    def __init__(self, client, name):
+        """Initialize the Clementine device."""
+        self._client = client
+        self._attr_name = name
 
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._name
+    def update(self) -> None:
+        """Retrieve the latest data from the Clementine Player."""
+        try:
+            client = self._client
 
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._state
+            if client.state == "Playing":
+                self._attr_state = MediaPlayerState.PLAYING
+            elif client.state == "Paused":
+                self._attr_state = MediaPlayerState.PAUSED
+            elif client.state == "Disconnected":
+                self._attr_state = MediaPlayerState.OFF
+            else:
+                self._attr_state = MediaPlayerState.PAUSED
 
-    @property
-    def volume_level(self):
-        """Return the volume level of the device."""
-        return self._volume_level
+            if client.last_update and (time.time() - client.last_update > 40):
+                self._attr_state = MediaPlayerState.OFF
 
-    @property
-    def is_volume_muted(self):
-        """Return true if the device is muted."""
-        return self._is_muted
+            volume = float(client.volume) if client.volume else 0.0
+            self._attr_volume_level = volume / 100.0
+            if client.active_playlist_id in client.playlists:
+                self._attr_source = client.playlists[client.active_playlist_id]["name"]
+            else:
+                self._attr_source = "Unknown"
+            self._attr_source_list = [s["name"] for s in client.playlists.values()]
 
-    @property
-    def supported_features(self):
-        """Flag media player features that are supported."""
-        return SUPPORT_AIRMUSIC
+            if client.current_track:
+                self._attr_media_title = client.current_track["title"]
+                self._attr_media_artist = client.current_track["track_artist"]
+                self._attr_media_album_name = client.current_track["track_album"]
+                self._attr_media_image_hash = client.current_track["track_id"]
+            else:
+                self._attr_media_image_hash = None
 
-    def turn_on(self):
-        """Turn on the media player."""
-        _LOGGER.debug("Turning on the player")
-        self._state = STATE_PLAYING
-        self.schedule_update_ha_state()
+        except Exception:
+            self._attr_state = MediaPlayerState.OFF
+            raise
 
-    def turn_off(self):
-        """Turn off the media player."""
-        _LOGGER.debug("Turning off the player")
-        self._state = STATE_IDLE
-        self.schedule_update_ha_state()
+    def select_source(self, source: str) -> None:
+        """Select input source."""
+        client = self._client
+        sources = [s for s in client.playlists.values() if s["name"] == source]
+        if len(sources) == 1:
+            client.change_song(sources[0]["id"], 0)
 
-    def set_volume_level(self, volume):
-        """Set the volume level."""
-        _LOGGER.debug("Setting volume to %s", volume)
-        self._volume_level = volume
-        # self._device.set_volume(volume)
-        self.schedule_update_ha_state()
+    async def async_get_media_image(self) -> tuple[bytes | None, str | None]:
+        """Fetch media image of current playing image."""
+        if self._client.current_track:
+            image = bytes(self._client.current_track["art"])
+            return (image, "image/png")
 
-    def mute_volume(self, mute):
-        """Mute the volume."""
-        _LOGGER.debug("Muting volume: %s", mute)
-        self._is_muted = mute
-        # self._device.mute(mute)
-        self.schedule_update_ha_state()
+        return None, None
 
-    def media_play(self):
+    def volume_up(self) -> None:
+        """Volume up the media player."""
+        newvolume = min(self._client.volume + 4, 100)
+        self._client.set_volume(newvolume)
+
+    def volume_down(self) -> None:
+        """Volume down media player."""
+        newvolume = max(self._client.volume - 4, 0)
+        self._client.set_volume(newvolume)
+
+    def mute_volume(self, mute: bool) -> None:
+        """Send mute command."""
+        self._client.set_volume(0)
+
+    def set_volume_level(self, volume: float) -> None:
+        """Set volume level."""
+        self._client.set_volume(int(100 * volume))
+
+    def media_play_pause(self) -> None:
+        """Simulate play pause media player."""
+        if self.state == MediaPlayerState.PLAYING:
+            self.media_pause()
+        else:
+            self.media_play()
+
+    def media_play(self) -> None:
         """Send play command."""
-        _LOGGER.debug("Playing media")
-        self._state = STATE_PLAYING
-        # self._device.play()
-        self.schedule_update_ha_state()
+        self._attr_state = MediaPlayerState.PLAYING
+        self._client.play()
 
-    def media_pause(self):
-        """Send pause command."""
-        _LOGGER.debug("Pausing media")
-        self._state = STATE_IDLE
-        # self._device.pause()
-        self.schedule_update_ha_state()
+    def media_pause(self) -> None:
+        """Send media pause command to media player."""
+        self._attr_state = MediaPlayerState.PAUSED
+        self._client.pause()
 
-    def media_stop(self):
-        """Send stop command."""
-        _LOGGER.debug("Stopping media")
-        self._state = STATE_IDLE
-        # self._device.stop()
-        self.schedule_update_ha_state()
-
-    def media_previous_track(self):
-        """Send previous track command."""
-        _LOGGER.debug("Playing previous track")
-        # self._device.previous_track()
-        self.schedule_update_ha_state()
-
-    def media_next_track(self):
+    def media_next_track(self) -> None:
         """Send next track command."""
-        _LOGGER.debug("Playing next track")
-        # self._device.next_track()
-        self.schedule_update_ha_state()
+        self._client.next()
+
+    def media_previous_track(self) -> None:
+        """Send the previous track command."""
+        self._client.previous()
