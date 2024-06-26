c2d17ec9f511f845d1c95682eae793b72320f174
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-26 21:43:02 +0200

    Update media_player.py

--- custom_components/airmusic/media_player.py
+++ custom_components/airmusic/media_player.py
@@ -10,16 +10,25 @@ from homeassistant.components.media_player.const import (
     SUPPORT_STOP,
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_MUTE,
+    SUPPORT_SELECT_SOURCE,
 )
 from homeassistant.const import STATE_IDLE, STATE_PLAYING, STATE_PAUSED
 
-from . import airmusic
+from .airmusic import airmusic
 
 _LOGGER = logging.getLogger(__name__)
 
 SUPPORT_AIRMUSIC = (
-    SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_STOP | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE
+    SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_STOP | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | SUPPORT_SELECT_SOURCE
 )
+
+SOURCES = {
+    "FM Radio": airmusic.KEY_MODE,
+    "DAB+ Radio": airmusic.KEY_MODE,
+    "Internet Radio": airmusic.KEY_INTERNETRADIO,
+    "AUX": airmusic.KEY_MODE,
+    "Network Device": airmusic.KEY_MODE,
+}
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the AirMusic media player platform."""
@@ -29,7 +38,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     ip_address = hass.data['airmusic']['ip_address']
     token = hass.data['airmusic']['token']
 
-    add_entities([airmusicDevice(ip_address, token)])
+    add_entities([AirMusicDevice(ip_address, token)])
 
 class AirMusicDevice(MediaPlayerEntity):
     """Representation of an AirMusic device."""
@@ -41,7 +50,8 @@ class AirMusicDevice(MediaPlayerEntity):
         self._state = STATE_IDLE
         self._volume = 0
         self._muted = False
-        self._airmusic = airmusic.airmusic(ip_address, token)
+        self._source = None
+        self._airmusic = airmusic(ip_address)
 
     @property
     def name(self):
@@ -68,12 +78,23 @@ class AirMusicDevice(MediaPlayerEntity):
         """Flag media player features that are supported."""
         return SUPPORT_AIRMUSIC
 
+    @property
+    def source(self):
+        """Return the current source."""
+        return self._source
+
+    @property
+    def source_list(self):
+        """List of available input sources."""
+        return list(SOURCES.keys())
+
     def update(self):
         """Fetch new state data for this media player."""
         status = self._airmusic.get_status()
         self._state = STATE_PLAYING if status == 'playing' else STATE_IDLE
         self._volume = self._airmusic.get_volume() / 100
         self._muted = self._airmusic.is_muted()
+        # Assuming there's a way to get the current source, set self._source accordingly
 
     def media_play(self):
         """Send play command."""
@@ -99,4 +120,10 @@ class AirMusicDevice(MediaPlayerEntity):
         """Mute (true) or unmute (false) media player."""
         self._airmusic.mute(mute)
         self._muted = mute
+
+    def select_source(self, source):
+        """Select input source."""
+        if source in SOURCES:
+            self._airmusic.press_key(SOURCES[source])
+            self._source = source
 
