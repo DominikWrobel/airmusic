a8bc1230da48c0372d4c9053842f0e0708142b59
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-26 14:53:26 +0200

    Update __init__.py

--- custom_components/airmusic/__init__.py
+++ custom_components/airmusic/__init__.py
@@ -1,1 +1,42 @@
+# custom_components/airmusic/__init__.py
 
+import logging
+import asyncio
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType
+from homeassistant.helpers import discovery
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "airmusic"
+
+async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
+    """Set up the AirMusic component."""
+    _LOGGER.info("Setting up AirMusic component")
+    
+    # This is where any initial setup would occur if necessary
+    # For example, setting up any global variables, configurations, or initializing APIs
+
+    # Example: if your component needs to set up a connection or something similar
+    # if not await async_setup_airmusic(hass, config):
+    #     return False
+
+    # Load platforms supported by this component
+    hass.async_create_task(
+        discovery.async_load_platform(hass, "media_player", DOMAIN, {}, config)
+    )
+
+    return True
+
+# Uncomment and implement if there is an async_setup_entry
+# async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+#     """Set up AirMusic from a config entry."""
+#     _LOGGER.info("Setting up AirMusic from config entry")
+#     return await async_setup(hass, entry.data)
+
+# Uncomment and implement if there is an async_unload_entry
+# async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+#     """Unload a config entry."""
+#     _LOGGER.info("Unloading AirMusic config entry")
+#     return True
