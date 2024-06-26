4bb408635b95bd6e0c33a64fb752d0c67a1dce23
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-26 14:57:58 +0200

    Update __init__.py

--- custom_components/airmusic/__init__.py
+++ custom_components/airmusic/__init__.py
@@ -6,25 +6,43 @@ import asyncio
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.const import CONF_HOST, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "airmusic"
 
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_HOST): cv.string,
+                vol.Optional(CONF_NAME, default="AirMusic"): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
 async def async_setup(hass: HomeAssistantType, config: ConfigType) -> bool:
     """Set up the AirMusic component."""
     _LOGGER.info("Setting up AirMusic component")
     
-    # This is where any initial setup would occur if necessary
-    # For example, setting up any global variables, configurations, or initializing APIs
+    if DOMAIN not in config:
+        return True
 
-    # Example: if your component needs to set up a connection or something similar
-    # if not await async_setup_airmusic(hass, config):
-    #     return False
+    conf = config[DOMAIN]
+    host = conf[CONF_HOST]
+    name = conf[CONF_NAME]
+
+    # Pass configuration to the platform
+    hass.data[DOMAIN] = conf
 
     # Load platforms supported by this component
     hass.async_create_task(
-        discovery.async_load_platform(hass, "media_player", DOMAIN, {}, config)
+        discovery.async_load_platform(hass, "media_player", DOMAIN, conf, config)
     )
 
     return True
