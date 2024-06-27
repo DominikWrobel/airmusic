613d1357488b7d89fa2719e3cd664f75301a5322
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-27 11:52:38 +0200

    Update media_player.py

--- custom_components/airmusic/media_player.py
+++ custom_components/airmusic/media_player.py
@@ -38,19 +38,20 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     ip_address = hass.data[DOMAIN][CONF_HOST]
 
-    async_add_entities([AirMusicDevice(ip_address)])
+    async_add_entities([AirMusicDevice(hass, ip_address)])
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up AirMusic from a config entry."""
     config = hass.data[DOMAIN][entry.entry_id]
     ip_address = config[CONF_HOST]
 
-    async_add_entities([AirMusicDevice(ip_address)])
+    async_add_entities([AirMusicDevice(hass, ip_address)])
 
 class AirMusicDevice(MediaPlayerEntity):
     """Representation of an AirMusic device."""
 
-    def __init__(self, ip_address):
+    def __init__(self, hass, ip_address):
+        self._hass = hass
         self._ip_address = ip_address
         self._state = STATE_IDLE
         self._volume = 0
@@ -86,36 +87,36 @@ class AirMusicDevice(MediaPlayerEntity):
     def source_list(self):
         return list(SOURCES.keys())
 
-    def update(self):
+    async def async_update(self):
         try:
-            status = self._airmusic.get_status()
+            status = await self._hass.async_add_executor_job(self._airmusic.get_status)
             self._state = STATE_PLAYING if status == 'playing' else STATE_IDLE
-            self._volume = self._airmusic.get_volume() / 100
-            self._muted = self._airmusic.is_muted()
+            self._volume = await self._hass.async_add_executor_job(self._airmusic.get_volume) / 100
+            self._muted = await self._hass.async_add_executor_job(self._airmusic.is_muted)
         except Exception as e:
             _LOGGER.error(f"Error updating AirMusic device: {e}")
 
-    def media_play(self):
-        if self._airmusic.play():
+    async def async_media_play(self):
+        if await self._hass.async_add_executor_job(self._airmusic.play):
             self._state = STATE_PLAYING
 
-    def media_pause(self):
-        if self._airmusic.pause():
+    async def async_media_pause(self):
+        if await self._hass.async_add_executor_job(self._airmusic.pause):
             self._state = STATE_PAUSED
 
-    def media_stop(self):
-        if self._airmusic.stop():
+    async def async_media_stop(self):
+        if await self._hass.async_add_executor_job(self._airmusic.stop):
             self._state = STATE_IDLE
 
-    def set_volume_level(self, volume):
-        if self._airmusic.set_volume(int(volume * 100)):
+    async def async_set_volume_level(self, volume):
+        if await self._hass.async_add_executor_job(self._airmusic.set_volume, int(volume * 100)):
             self._volume = volume
 
-    def mute_volume(self, mute):
-        if self._airmusic.mute(mute):
+    async def async_mute_volume(self, mute):
+        if await self._hass.async_add_executor_job(self._airmusic.mute, mute):
             self._muted = mute
 
-    def select_source(self, source):
+    async def async_select_source(self, source):
         if source in SOURCES:
-            if self._airmusic.press_key(SOURCES[source]):
+            if await self._hass.async_add_executor_job(self._airmusic.press_key, SOURCES[source]):
                 self._source = source
