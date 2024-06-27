a9ffaffb523551ecf7bb9a79d8acb0294268dc51
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-27 12:25:50 +0200

    Update airmusic.py

--- custom_components/airmusic/airmusic.py
+++ custom_components/airmusic/airmusic.py
@@ -7,42 +7,13 @@ _LOGGER = logging.getLogger(__name__)
 
 class airmusic:
     # Define the constants
-    KEY_HOME = 1
-    KEY_UP = 2
-    KEY_DOWN = 3
-    KEY_LEFT = 4
-    KEY_RIGHT = 5
-    KEY_ENTER = 6
-    KEY_POWER = 7  # Toggle power on/off.
-    KEY_MUTE = 8
-    KEY_VOLUP = 9  # Volume up one step.
-    KEY_VOLDOWN = 10  # Volume down one step.
-    KEY_ALARMCLOCK = 11
-    KEY_SLEEPTIMER = 12
-    KEY_LANGUAGE = 13  # Open the language menu.
-    KEY_SCREENDIM = 14  # Toggle screen dim on/off.
-    KEY_CHANNELFAV = 15  # Show the favourites menu.
-    KEY_BUTTON0 = 17
-    KEY_BUTTON1 = 18
-    KEY_BUTTON2 = 19
-    KEY_BUTTON3 = 20
-    KEY_BUTTON4 = 21
-    KEY_BUTTON5 = 22
-    KEY_BUTTON6 = 23
-    KEY_BUTTON7 = 24
-    KEY_BUTTON8 = 25
-    KEY_BUTTON9 = 26
-    KEY_MODE = 28  # Toggle between the device modes: FM, IRadio, USB, AUX, UPNP, ...
-    KEY_STOP = 30  # Stop playing a song / station.
-    KEY_NEXT = 31  # Go to the next item.
-    KEY_PREV = 32  # Go to the next item.
-    KEY_USB = 36  # Swith to USB mode.
-    KEY_INTERNETRADIO = 40  # Switch to IRadio mode.
-    KEY_POWERSAVING = 105  # Go to the power saving menu, item 'Turn On'.
-    KEY_EQ_FLAT = 106  # Select "Flat" equaliser mode.
-    KEY_SYSTEMMENU = 110  # Go to the system menu.
-    KEY_WPS = 111  # Start WPS mode.
-    KEY_NEXTFAV = 112  # Go to the next station in the favourites list.
+    KEY_MODE = 'KEY_MODE'
+    KEY_INTERNETRADIO = 'KEY_INTERNETRADIO'
+    KEY_PLAY = 'KEY_PLAY'
+    KEY_PAUSE = 'KEY_PAUSE'
+    KEY_STOP = 'KEY_STOP'
+    KEY_MUTE = 'KEY_MUTE'
+    KEY_UNMUTE = 'KEY_UNMUTE'
 
     DEFAULT_AUTH = 'c3UzZzRnbzZzazc6amkzOTQ1NHh1L14='
 
@@ -65,7 +36,7 @@ class airmusic:
             return False
 
     def get_status(self):
-        url = f"http://{self._device_address}/init"
+        url = f"http://{self._device_address}/status"
         headers = {
             "Authorization": f"Basic {self.DEFAULT_AUTH}"
         }
