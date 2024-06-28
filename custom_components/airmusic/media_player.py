97106666e50b7302e0dca7444add55743cd614ff
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-06-28 21:23:21 +0200

    Update media_player.py

--- custom_components/airmusic/media_player.py
+++ custom_components/airmusic/media_player.py
@@ -11,6 +11,10 @@ from homeassistant.components.media_player.const import (
     SUPPORT_VOLUME_SET,
     SUPPORT_VOLUME_MUTE,
     SUPPORT_SELECT_SOURCE,
+    SUPPORT_TURN_ON,
+    SUPPORT_TURN_OFF,
+    SUPPORT_PLAY_MEDIA,
+    SUPPORT_BROWSE_MEDIA
 )
 from homeassistant.const import STATE_IDLE, STATE_PLAYING, STATE_PAUSED, CONF_HOST  # Import CONF_HOST
 
@@ -20,12 +24,12 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 SUPPORT_AIRMUSIC = (
-    SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_STOP | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | SUPPORT_SELECT_SOURCE
+    SUPPORT_PLAY | SUPPORT_PAUSE | SUPPORT_STOP | SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | SUPPORT_SELECT_SOURCE | SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_PLAY_MEDIA | SUPPORT_BROWSE_MEDIA
 )
 
 SOURCES = {
-    "FM Radio": airmusic.KEY_MODE,
-    "DAB+ Radio": airmusic.KEY_MODE,
+#    "FM Radio": airmusic.KEY_MODE,
+#    "DAB+ Radio": airmusic.KEY_MODE,
     "Internet Radio": airmusic.KEY_INTERNETRADIO,
     "AUX": airmusic.KEY_MODE,
     "Network Device": airmusic.KEY_MODE,
@@ -58,6 +62,9 @@ class AirMusicDevice(MediaPlayerEntity):
         self._muted = False
         self._source = None
         self._airmusic = airmusic(ip_address, timeout = 10)
+        self._turn_off = self.turn_off
+        self._turn_on = self.turn_on
+        self._stop = self.stop
 
     @property
     def name(self):
@@ -84,17 +91,38 @@ class AirMusicDevice(MediaPlayerEntity):
         return self._source
 
     @property
+    def turn_on(self):
+        return self._turn_on
+        
+    @property
+    def stop(self):
+        return self.stop
+        
+    @property
+    def turn_on(self):
+        return self._turn_on
+
+    @property
     def source_list(self):
         return list(SOURCES.keys())
-
+        
     async def async_update(self):
         try:
-            status = await self._hass.async_add_executor_job(self._airmusic.get_status)
-            self._state = STATE_PLAYING if status == 'playing' else STATE_IDLE
-            self._volume = await self._hass.async_add_executor_job(self._airmusic.get_volume) / 100
-            self._muted = await self._hass.async_add_executor_job(self._airmusic.get_mute()
+            status = await self._hass.async_add_executor_job(self._airmusic.get_playinfo)
+            self._state = STATE_PLAYING if int(self._airmusic.get_playinfo.sid == '6') else STATE_IDLE
+            self._muted = await self._hass.async_add_executor_job(self._airmusic.get_mute)
+            self._volume = await self._hass.async_add_executor_job(self._airmusic.get_volume) / 30
         except Exception as e:
             _LOGGER.error(f"Error updating AirMusic device: {e}")
+
+#    async def async_update(self):
+#        try:
+#            status = await self._hass.async_add_executor_job(self._airmusic.get_status)
+#            self._state = STATE_PLAYING if status == 'playing' else STATE_IDLE
+#            self._volume = await self._hass.async_add_executor_job(self._airmusic.get_volume) / 100
+#            self._muted = await self._hass.async_add_executor_job(self._airmusic.get_mute())
+#        except Exception as e:
+#            _LOGGER.error(f"Error updating AirMusic device: {e}")
 
     async def async_media_play(self):
         if await self._hass.async_add_executor_job(self._airmusic.play):
@@ -109,17 +137,20 @@ class AirMusicDevice(MediaPlayerEntity):
             self._state = STATE_IDLE
 
     async def async_set_volume_level(self, volume):
-        if await self._hass.async_add_executor_job(self._airmusic.set_volume, int(volume * 100)):
+        if await self._hass.async_add_executor_job(self._airmusic.set_volume, int (volume * 30)):
             self._volume = volume
 
     def mute_volume(self, mute):
         self._airmusic.mute = mute
+        
+    def stop (self, stop):
+        self._airmusic.stop = stop
+        
+    def turn_on(self, turn_on):
+        self._airmusic.turn_on = turn_on
 
-#    async def async_update(self):
-#        try:
-#            self._muted = await self._hass.async_add_executor_job(self._airmusic.muted)
-#        except Exception as e:
-#            _LOGGER.error(f"Error updating AirMusic device: {e}")
+    def turn_off(self, turn_off):
+        self._airmusic.turn_off = turn_off
 
     async def async_select_source(self, source):
         if source in SOURCES:
