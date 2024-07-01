123152604acac473028720805d11ef3a8fb90a18
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-07-02 00:56:55 +0200

    Update __init__.py

--- custom_components/airmusic/__init__.py
+++ custom_components/airmusic/__init__.py
@@ -1,52 +1,187 @@
+"""
+Support for Airmusic Internet Radios.
+"""
+
 import logging
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import discovery
-from homeassistant.const import CONF_HOST, CONF_NAME
+import urllib.parse
+import urllib.request
+import aiohttp
+import asyncio
+import voluptuous as vol
 
-from .const import DOMAIN
+from homeassistant.const import (
+    CONF_DEVICES, CONF_HOST, CONF_NAME, CONF_PASSWORD, CONF_PORT, CONF_TIMEOUT,
+    CONF_USERNAME)
+from homeassistant.helpers import config_validation as cv, discovery
+from homeassistant.helpers.entity import Entity
 
+# VERSION
+VERSION = '0.1'
+
+# REQUIREMENTS
+REQUIREMENTS = ['beautifulsoup4==4.6.3']
+
+# LOGGING
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the AirMusic component."""
-    if DOMAIN not in config:
-        return True
+# DOMAIN
+DOMAIN = 'airmusic'
 
-    conf = config[DOMAIN]
-    devices = conf.get('devices', [])
+# Supported domains
+SUPPORTED_DOMAINS = ['media_player']
 
-    hass.data.setdefault(DOMAIN, {})
+# DEFAULTS
+DEFAULT_PORT = 8080
+DEFAULT_NAME = "Airmusic Radio"
+DEFAULT_TIMEOUT = 30
+DEFAULT_USERNAME = 'roosu3g4go6sk7'
+DEFAULT_PASSWORD = 'ji39454xu/^'
+DEFAULT_SOURCE = ''
+DEFAULT_IMAGE = 'logo'
 
-    for device in devices:
-        ip_address = device.get(CONF_HOST)
-        name = device.get(CONF_NAME)
+# Local
+CONF_SOURCE = 'source'
+CONF_IMAGE = 'image'
+CONF_OPENER = ''
 
-        hass.data[DOMAIN][ip_address] = {
-            CONF_HOST: ip_address,
-            CONF_NAME: name,
-        }
+AIRMUSIC_CONFIG = vol.Schema({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_USERNAME, default=DEFAULT_USERNAME): cv.string,
+    vol.Optional(CONF_PASSWORD, default=DEFAULT_PASSWORD): cv.string,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.socket_timeout,
+    vol.Optional(CONF_SOURCE, default=DEFAULT_SOURCE): cv.string,
+    vol.Optional(CONF_IMAGE, default=DEFAULT_IMAGE): cv.string,
+    })
 
-        await hass.async_create_task(
-            discovery.async_load_platform(hass, 'media_player', DOMAIN, {
-                CONF_HOST: ip_address,
-                CONF_NAME: name,
-            }, config)
-        )
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_DEVICES):
+            vol.All(cv.ensure_list, [
+                vol.Schema({
+                    vol.Required(CONF_HOST): cv.string,
+                    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+                    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                    vol.Optional(CONF_USERNAME, default=DEFAULT_USERNAME):
+                        cv.string,
+                    vol.Optional(CONF_PASSWORD, default=DEFAULT_PASSWORD):
+                        cv.string,
+                    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT):
+                        cv.socket_timeout,
+                    vol.Optional(CONF_SOURCE, default=DEFAULT_SOURCE):
+                        cv.string,
+                    vol.Optional(CONF_IMAGE, default=DEFAULT_IMAGE):
+                        cv.string,
+                }),
+            ]),
+        })
+}, extra=vol.ALLOW_EXTRA)
+
+
+async def async_setup(hass, config):
+    """Initialize the Airmusic device."""
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = []
+
+    _LOGGER.info("Initializing Airmusic devices")
+
+    airmusic_list = []
+
+    configured_devices = config[DOMAIN].get(CONF_DEVICES)
+    for device in configured_devices:
+        airmusic = AirmusicDevice(device.get(CONF_HOST),
+                                device.get(CONF_PORT),
+                                device.get(CONF_NAME),
+                                device.get(CONF_USERNAME),
+                                device.get(CONF_PASSWORD),
+                                device.get(CONF_TIMEOUT),
+                                device.get(CONF_SOURCE),
+                                device.get(CONF_IMAGE))
+
+        _LOGGER.debug("Airmusic device %s configured", device.get(CONF_HOST))
+        airmusic_list.append(airmusic)
+
+    hass.data[DOMAIN] = airmusic_list
+
+    if not airmusic_list:
+        _LOGGER.info("No airmusic devices configured")
+        return False
+
+    _LOGGER.debug("Configured %s airmusic", len(airmusic_list))
+
+    for domain in SUPPORTED_DOMAINS:
+        hass.async_create_task(
+            discovery.async_load_platform(hass, domain, DOMAIN, {}, config))
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up AirMusic from a config entry."""
-    _LOGGER.debug(f"Setting up entry: {entry.data}")
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setup(entry, 'media_player')
-    return True
+class AirmusicDevice(Entity):
+    """Representation of a Airmusic device."""
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    _LOGGER.debug(f"Unloading entry: {entry.entry_id}")
-    await hass.config_entries.async_forward_entry_unload(entry, 'media_player')
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    def __init__(self, host, port, name, username, password, timeout,
+                image, source):
+        """Initialize the Airmusic device."""
+        self._host = host
+        self._port = port
+        self._name = name
+        self._username = username
+        self._password = password
+        self._timeout = timeout
+        self._source = source
+        self._image = image
+        self._pwstate = True
+        self._volume = 0
+        self._muted = False
+        self._selected_source = ''
+        self._image_url = None
+        self._source_names = {}
+        self._sources = {}
+        # Opener for http connection
+        self._opener = aiohttp.ClientSession()
+
+    @property
+    def get_host(self):
+        """Return the host of the device."""
+        return self._host
+
+    @property
+    def get_port(self):
+        """Return the port of the device."""
+        return self._port
+
+    @property
+    def get_name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def get_username(self):
+        """Return the username of the device."""
+        return self._username
+
+    @property
+    def get_password(self):
+        """Return the password of the device."""
+        return self._password
+
+    @property
+    def get_timeout(self):
+        """Return the timeout of the device."""
+        return self._timeout
+
+    @property
+    def get_source(self):
+        """Return the favorites of the device."""
+        return self._source
+
+    @property
+    def get_image(self):
+        """Return the image of the device."""
+        return self._image
+
+    @property
+    def get_opener(self):
+        """Return the socket of the device."""
+        return self._opener
