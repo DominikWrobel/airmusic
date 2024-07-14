7d79f9f96af5518a97a37ed24312a9d8b99248ee
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-07-14 21:12:48 +0200

    Update __init__.py

--- custom_components/airmusic/__init__.py
+++ custom_components/airmusic/__init__.py
@@ -21,7 +21,7 @@ from .const import DOMAIN, CONF_HOST, CONF_NAME
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = '1.0'
+VERSION = '1.1'
 
 # REQUIREMENTS
 REQUIREMENTS = ['beautifulsoup4==4.6.3']
